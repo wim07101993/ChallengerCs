463a5e00a43f4d24e44b92c507429083b76892eb
wim07101993 <van.laer.wim@gmail.com>
2020-06-01 22:10:47 +0200

    Added converter methods

--- DataConverter.Core/Converters/DotNetConverter.cs
+++ DataConverter.Core/Converters/DotNetConverter.cs
@@ -2,5 +2,66 @@
 {
     public class DotNetConverter : IConverter
     {
+        public string GetAsciiString(byte[] bytes) => throw new System.NotImplementedException();
+
+        public decimal GetBCD(byte[] bytes) => throw new System.NotImplementedException();
+
+        public byte[] GetBCDBytes(decimal d) => throw new System.NotImplementedException();
+
+        public string GetBinaryString(byte[] bytes) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(ushort s) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(uint i) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(ulong l) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(short s) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(int i) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(long l) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(float f) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(double d) => throw new System.NotImplementedException();
+
+        public byte[] GetBytes(decimal d) => throw new System.NotImplementedException();
+
+        public decimal GetDecimal(byte[] bytes) => throw new System.NotImplementedException();
+
+        public string GetDecimalString(byte[] bytes) => throw new System.NotImplementedException();
+
+        public double GetDoulbe(byte[] bytes) => throw new System.NotImplementedException();
+
+        public float GetFloat(byte[] bytes) => throw new System.NotImplementedException();
+
+        public string GetHexString(byte[] bytes) => throw new System.NotImplementedException();
+
+        public int GetInt(byte[] bytes) => throw new System.NotImplementedException();
+
+        public long GetLong(byte[] bytes) => throw new System.NotImplementedException();
+
+        public string GetOctalString(byte[] bytes) => throw new System.NotImplementedException();
+
+        public short GetShort(byte[] bytes) => throw new System.NotImplementedException();
+
+        public uint GetUInt(byte[] bytes) => throw new System.NotImplementedException();
+
+        public ulong GetULong(byte[] bytes) => throw new System.NotImplementedException();
+
+        public ushort GetUShort(byte[] bytes) => throw new System.NotImplementedException();
+
+        public string GetUtf32String(byte[] bytes) => throw new System.NotImplementedException();
+
+        public string GetUtf8String(byte[] bytes) => throw new System.NotImplementedException();
+
+        public byte[] ParseBinaryString(string s) => throw new System.NotImplementedException();
+
+        public byte[] ParseDecimalString(string s) => throw new System.NotImplementedException();
+
+        public byte[] ParseHexadecimalString(string s) => throw new System.NotImplementedException();
+
+        public byte[] ParseOctalString(string s) => throw new System.NotImplementedException();
     }
 }
---
+++ DataConverter.Core/Converters/ObfuscatedConverter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DataConverter.Core.Converters
+{
+    public class ObfuscatedConverter : IConverter
+    {
+        public string GetAsciiString(byte[] bytes) => throw new NotImplementedException();
+
+        public decimal GetBCD(byte[] bytes) => throw new NotImplementedException();
+
+        public byte[] GetBCDBytes(decimal d) => throw new NotImplementedException();
+
+        public string GetBinaryString(byte[] bytes) => throw new NotImplementedException();
+
+        public byte[] GetBytes(ushort s) => throw new NotImplementedException();
+
+        public byte[] GetBytes(uint i) => throw new NotImplementedException();
+
+        public byte[] GetBytes(ulong l) => throw new NotImplementedException();
+
+        public byte[] GetBytes(short s) => throw new NotImplementedException();
+
+        public byte[] GetBytes(int i) => throw new NotImplementedException();
+
+        public byte[] GetBytes(long l) => throw new NotImplementedException();
+
+        public byte[] GetBytes(float f) => throw new NotImplementedException();
+
+        public byte[] GetBytes(double d) => throw new NotImplementedException();
+
+        public byte[] GetBytes(decimal d) => throw new NotImplementedException();
+
+        public char GetChar(byte b) => throw new NotImplementedException();
+
+        public decimal GetDecimal(byte[] bytes) => throw new NotImplementedException();
+
+        public string GetDecimalString(byte[] bytes) => throw new NotImplementedException();
+
+        public double GetDoulbe(byte[] bytes) => throw new NotImplementedException();
+
+        public float GetFloat(byte[] bytes) => throw new NotImplementedException();
+
+        public string GetHexString(byte[] bytes) => throw new NotImplementedException();
+
+        public int GetInt(byte[] bytes) => throw new NotImplementedException();
+
+        public long GetLong(byte[] bytes) => throw new NotImplementedException();
+
+        public string GetOctalString(byte[] bytes) => throw new NotImplementedException();
+
+        public short GetShort(byte[] bytes) => throw new NotImplementedException();
+
+        public uint GetUInt(byte[] bytes) => throw new NotImplementedException();
+
+        public ulong GetULong(byte[] bytes) => throw new NotImplementedException();
+
+        public ushort GetUShort(byte[] bytes) => throw new NotImplementedException();
+
+        public string GetUtf32String(byte[] bytes) => throw new NotImplementedException();
+
+        public string GetUtf8String(byte[] bytes) => throw new NotImplementedException();
+
+        public byte[] ParseBinaryString(string s) => throw new NotImplementedException();
+
+        public byte[] ParseDecimalString(string s) => throw new NotImplementedException();
+
+        public byte[] ParseHexadecimalString(string s) => throw new NotImplementedException();
+
+        public byte[] ParseOctalString(string s) => throw new NotImplementedException();
+    }
+}
