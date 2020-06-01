f1b808ef9fa4d292a387418a43ab2de14921c9c0
wim07101993 <van.laer.wim@gmail.com>
2020-06-01 21:31:38 +0200

    created core, benchmark and test project

---
+++ DataConverter.Core.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DataConverter.Core.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main() => Console.WriteLine("Hello benchmark!");
+    }
+}
