fcdc93c37b52fcb518cd42f9a8472f4b586e9917
wim07101993 <van.laer.wim@gmail.com>
2020-06-03 18:49:41 +0200

    created application projects

---
+++ DataConverter.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataConverter.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DataConverter.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataConverter.Shared
+{
+    public class Class1
+    {
+    }
+}
